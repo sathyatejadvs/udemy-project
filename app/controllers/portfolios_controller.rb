a9692902710ace6caa1f338ac689e50844cffedf
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-06-24 15:29:18 +0530

    Added update and create functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,7 +16,23 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  def edit
+  	 @portfolio_item = Portfolio.find(params[:id])
+
+  end
+  def show
+  end
+  def update
+  	   @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(port_params)
+        format.html { redirect_to portfolios_path, notice: 'Article successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
   def port_params
-      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image, :id)
     end
 end
