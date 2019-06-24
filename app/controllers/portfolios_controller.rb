180c3fbb40613014eaf909e6b95989c57ad52c3b
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-06-24 16:16:40 +0530

    Created show button for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,6 +21,8 @@ class PortfoliosController < ApplicationController
 
   end
   def show
+  	  @portfolio_item = Portfolio.find(params[:id])
+
   end
   def update
   	   @portfolio_item = Portfolio.find(params[:id])
