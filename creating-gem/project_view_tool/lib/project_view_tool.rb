ede06a6864375403e86abd38ab4aeb6013d7a95d
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-01 12:37:15 +0530

    Project view tool initial commit

---
+++ creating-gem/project_view_tool/lib/project_view_tool.rb
@@ -0,0 +1,8 @@
+require "project_view_tool/version"
+require "project_view_tool/renderer"
+
+
+module ProjectViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ creating-gem/project_view_tool/lib/project_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ProjectViewTool
+  VERSION = "0.1.0"
+end
