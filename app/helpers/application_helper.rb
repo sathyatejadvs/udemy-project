a34bf2fd815c72111d09418821761a31b22566a6
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-03 10:51:05 +0530

    Rendered a footer partial to view action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-	def credentials_validation
+	def credentials_validation(style)
 		if current_user.is_a?(User) 
-			link_to'Logout' ,destroy_user_session_path, method: :delete
+			link_to'Logout' ,destroy_user_session_path, method: :delete, class: style
 		else 
-			(link_to'Register' , new_user_registration_path) + " <br>".html_safe  +  
-			(link_to'Login' , new_user_session_path) 
+			(link_to'Register' , new_user_registration_path ,class: style) + " ".html_safe  +  
+			(link_to'Login' , new_user_session_path ,class: style)
 		end
 	end
 
