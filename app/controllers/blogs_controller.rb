29060226f75779865a695df01654190c78c828ac
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-05 13:57:59 +0530

    Integrated Styling for the blog pages

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -77,6 +77,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params 
-      params.require(:blog).permit(topic_attributes: [:title, :body, :id])                   
+      params.require(:blog).permit(:title, :body)                   
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,8 @@ end
 10.times do |blog|
 	Blog.create!(
 		title: "my blog post #{blog}" ,
-		body: "Something went wrong!", 
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
 		topic_id: Topic.last.id
 	)
 end
