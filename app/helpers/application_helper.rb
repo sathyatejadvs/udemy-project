eaecf540c564fa126078e4b7dee3ba4d647787ac
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-08 13:58:01 +0530

    Implemented refactor for nav links
    - rendered navigation partial for blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,10 +45,14 @@ module ApplicationHelper
     nav_links = ''
 
     nav_items.each do |item|
-      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} '>#{item[:title]}</a></#{tag_type}>"
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]} '>#{item[:title]}</a></#{tag_type}>"
     end
 
     nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 
 end
