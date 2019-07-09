ef070eef53e05f6424bfdcd86c9fc425867b6ffb
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-09 14:36:54 +0530

    Implemented sortable for portfolio

--- app/assets/javascripts/drag_n_drop.js
+++ app/assets/javascripts/drag_n_drop.js
@@ -1,17 +1,7 @@
 // drag_n_drop
 
 $(document).ready(function() {
-  alert('kkkk')
 
-
-  $( "#draggable" ).draggable();
-  $( "#droppable" ).droppable({
-    drop: function( event, ui ) {
-      $( this )
-        .addClass( "ui-state-highlight" )
-        .find( "p" )
-          .html( "Dropped!" );
-    }
-  });
+  $("#sortables").sortable();
   
 });
