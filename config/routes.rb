d296ed9300eb6459c7717b3fb0c7d6a44bff778f
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-07-15 14:29:27 +0530

    Resolved the issues for routing
    - Refactored the logout bug happend in application.
    - Provided the edit link for portfolio show action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, path:'', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' ,}
+
+  devise_scope :user do 
+    get "register", :to => "devise/registrations#new", as: :register 
+    get "login", :to => "devise/sessions#new", as: :login
+    get "logout", :to => "devise/sessions#destroy", as: :logout
+  end
+  
   resources :portfolios, except: [:show]
   get 'angular-items', to:'portfolios#angular'
  	get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
